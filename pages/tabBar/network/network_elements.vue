1f8f870dcc561e0ab38725ec9256561145cde4c5
1758310694 <135298824+1758310694@users.noreply.github.com>
2025-05-08 16:46:01 +0800

    Add files via upload

---
+++ pages/tabBar/network/network_elements.vue
@@ -0,0 +1,26 @@
+<template>
+  <view class="container">
+    <web-view :src="htmlUrl"></web-view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      htmlUrl: ''
+    };
+  },
+  onLoad() {
+    // 调用后端接口获取 HTML 文件的 URL
+    this.htmlUrl = 'http://172.31.152.31:80//minerals_elements_network.html';
+  }
+};
+</script>
+
+<style>
+.container {
+  width: 100%;
+  height: 100%;
+}
+</style>
