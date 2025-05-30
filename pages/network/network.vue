04cb9e128e34ccaaaf693658e5729cee57ff952b
1758310694 <135298824+1758310694@users.noreply.github.com>
2025-05-30 21:48:26 +0800

    Update network.vue

--- pages/network/network.vue
+++ pages/network/network.vue
@@ -13,7 +13,7 @@ export default {
   },
   onLoad() {
     // 调用后端接口获取 HTML 文件的 URL
-    this.htmlUrl = 'http://172.31.152.31:80//minerals_network.html';
+    this.htmlUrl = 'https://app-wlbr.onrender.com//minerals_network.html';
   }
 };
 </script>
