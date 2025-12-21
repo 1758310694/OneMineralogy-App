a71b2e3575e7886be2bae8c8233b47df6e97a0d5
1758310694 <135298824+1758310694@users.noreply.github.com>
2025-12-21 16:02:13 +0800

    Update HTML URL for network page

--- pages/network/network.vue
+++ pages/network/network.vue
@@ -13,7 +13,7 @@ export default {
   },
   onLoad() {
     // 调用后端接口获取 HTML 文件的 URL
-    this.htmlUrl = 'https://app-wlbr.onrender.com//minerals_network.html';
+    this.htmlUrl = 'https://jewelry-change-establish-tabs.trycloudflare.com/minerals_network.html';
   }
 };
 </script>
